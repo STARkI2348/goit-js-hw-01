09f6cbf0352a5095a3923aea5d271129d34628d2
STARkI2348 <107957195+STARkI2348@users.noreply.github.com>
2024-05-01 21:54:07 +0300

    2

--- js/task-1.js
+++ js/task-1.js
@@ -1,16 +1,9 @@
-const value = 20;
 
-
-function say(v) {
-    const d = `${v}` >= 18;
-    console.log(d);
+function makeTransaction(quantity, pricePerDroid) {
+  const totalPrice = quantity * pricePerDroid;
+  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
-say(20);
+console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
 
-function add(a, b, c) {
-  console.log(`Addition result equals ${a + b + c} ` );
-}
-
-add(15, 27, 10);
-add(10, 20, 30);
-add(5, 10, 15);
