94c4afd1accab3106b1e329bf6324b91993dfce5
STARkI2348 <107957195+STARkI2348@users.noreply.github.com>
2024-05-01 20:37:34 +0300

    1

---
+++ js/task-1.js
@@ -0,0 +1,16 @@
+const value = 20;
+
+
+function say(v) {
+    const d = `${v}` >= 18;
+    console.log(d);
+}
+say(20);
+
+function add(a, b, c) {
+  console.log(`Addition result equals ${a + b + c} ` );
+}
+
+add(15, 27, 10);
+add(10, 20, 30);
+add(5, 10, 15);
