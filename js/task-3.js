2a464d9b6289048619c0d65f4c59c28a2dac6ce6
STARkI2348 <107957195+STARkI2348@users.noreply.github.com>
2024-05-05 11:59:30 +0300

    task_3

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,10 @@
 function getElementWidth(content, padding, border) { 
-    
-    const box =content + (padding * 2) + (border * 2);
+    let cont = Number.parseInt(content);
+    let pad = Number.parseInt(padding);
+    let bor = Number.parseInt(border);
+    let box = cont + (pad * 2) + (bor * 2);
     return box;
 }
-console.log(getElementWidth ("50px", "8px", "4px")); // 74
+console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
